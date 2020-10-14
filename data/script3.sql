1db44184e2d1658d2ca4927b382a9c650142de41
antonysama <antonysama@gmail.com>
2020-10-13 20:54:54 -0600

    modified how, added:df, script3

---
+++ data/script3.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS public.table;
+CREATE TABLE public.table (Prov char(2), GDP numeric, Year smallint);
+COPY public.table FROM '/var/lib/postgresql/data/data/df.csv' WITH CSV HEADER NULL 'NA';
+-- Sum GDP by Year and Prov, AVG can also  be used
+SELECT
+ Year,
+ SUM(GDP) FILTER (WHERE Prov='ON') AS "ON",
+ SUM(GDP) FILTER (WHERE Prov='AB') AS "AB"
+FROM public.table
+GROUP BY Year
+ORDER BY Year;
